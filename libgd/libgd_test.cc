e8886ef4b01cb1d9cb098d0f3760a22adb266336
LI Chen <lic3@wangsu.com>
2017-08-21 12:33:35 +0800

    use libgd in memory.

--- libgd/libgd_test.cc
+++ libgd/libgd_test.cc
@@ -3,28 +3,40 @@
 
 /* Bring in standard I/O so we can output the PNG to a file */
 #include <stdio.h>
-
+void ReadJpgFile(const char *filename, unsigned char *&buff, size_t &size)
+{
+  std::ifstream infile;
+  infile.open(filename);
+  infile.seekg(0, std::ios::end);
+  size = infile.tellg();
+  infile.seekg(0, std::ios::beg);
+  buff = new unsigned char[size];
+  infile.read((char *)buff, size);
+  infile.close();
+}
+void WriteJpgFile(const char *filename, unsigned char *buff, size_t size)
+{
+  std::ofstream fout; // create an ofstream object named fout
+  fout.open(filename,std::ios::trunc);
+  fout.write((char *)buff,size);
+  fout.close();
+}
 int main() {
+  size_t insize = 0;
+  size_t outsize = 0;
+  char *in_buf = NULL;
+  char *out_buf = NULL;
+  ReadJpgFile("1.jpg",in_buf,insize);
   /* Declare the image */
   gdImagePtr im;
-  /* Declare output files */
-  FILE *jpegout;
-  /* Declare color indexes */
-  if(gdSupportsFileType("1.jpg",0)) im = gdImageCreateFromFile("1.jpg");
-
-
-  /* Do the same for a JPEG-format file. */
-  jpegout = fopen("2.jpg", "wb");
-
-
-  /* Output the same image in JPEG format, using the default
-    JPEG quality setting. */
+  im = gdImageCreateFromJpegPtr(insize,in_buf);
   gdImageInterlace(im, 1);
-  gdImageJpeg(im, jpegout, 75);
+  out_buf = gdImageJpegPtr(im, &outsize, 75);
 
-  /* Close the files. */
-  fclose(jpegout);
 
+  WriteJpgFile("2.jpg",out_buf,outsize);
   /* Destroy the image in memory. */
   gdImageDestroy(im);
+  gdFree(out_buf);
+  delete in_buf;
 }
