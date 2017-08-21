8b9ae8c9cac010f55edd3fe1b28fb7b1083bcc19
LI Chen <lic3@wangsu.com>
2017-08-21 10:37:57 +0800

    try to progressive.

--- libgd/libgd_test.cc
+++ libgd/libgd_test.cc
@@ -14,11 +14,12 @@ int main() {
 
 
   /* Do the same for a JPEG-format file. */
-  jpegout = fopen("test.jpg", "wb");
+  jpegout = fopen("2.jpg", "wb");
 
 
   /* Output the same image in JPEG format, using the default
     JPEG quality setting. */
+  gdImageInterlace(im, 1);
   gdImageJpeg(im, jpegout, 75);
 
   /* Close the files. */
