3372d9cb2e78fbb36165976b0c5648d88944dccf
LI Chen <lic3@wangsu.com>
2017-08-21 10:18:36 +0800

    a simple test of libgd.

---
+++ libgd/libgd_test.cc
@@ -0,0 +1,29 @@
+/* Bring in gd library functions */
+#include "gd.h"
+
+/* Bring in standard I/O so we can output the PNG to a file */
+#include <stdio.h>
+
+int main() {
+  /* Declare the image */
+  gdImagePtr im;
+  /* Declare output files */
+  FILE *jpegout;
+  /* Declare color indexes */
+  if(gdSupportsFileType("1.jpg",0)) im = gdImageCreateFromFile("1.jpg");
+
+
+  /* Do the same for a JPEG-format file. */
+  jpegout = fopen("test.jpg", "wb");
+
+
+  /* Output the same image in JPEG format, using the default
+    JPEG quality setting. */
+  gdImageJpeg(im, jpegout, 75);
+
+  /* Close the files. */
+  fclose(jpegout);
+
+  /* Destroy the image in memory. */
+  gdImageDestroy(im);
+}
